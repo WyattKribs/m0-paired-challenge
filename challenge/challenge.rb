d3bf552a440e642b781b3f41ac0f1541b17c5e13
Wyatt Kribs <kribswyatt@gmail.com>
2022-03-01 20:49:03 -0700

    adds ruby file and modifies index

---
+++ challenge/challenge.rb
@@ -0,0 +1,8 @@
+#Library of books
+number_of_books = 100 #number of books in the library
+librarians = ["Jenn", "Hugh"] #librarians at the library
+library_open = true #the library is open!
+interesting_titles = ["Ender's Game", "All Quite on the Western Front", "Confederacy of Dunces", "The Hobbit", "Berserk"] #list of books I enjoy
+library_patrons = 14 #the number of people currently in the library
+bookshelves = 10.0 #number of shelves with books
+library_location = "Garfield County"
---
+++ challenge/index.md
@@ -0,0 +1,11 @@
+##Book Titles
+1. All Quite on the Western Front
+2. Confederacy of Dunces
+3. The Hobbit
+4. Ender's Game
+5. Berserk
+
+####Workflow
+My workflow started with coming up with the variables that I felt appropriate
+for the requirements of the challenge and that could feasibly be made to store
+the values appropriate. 
